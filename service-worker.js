bef087b7e6a4333e7f1033ac3494e122f89c0a0f
Moser <moritz.moser@bcw-gruppe.de>
2026-05-27 12:48:47 +0200

    feat(cars): Massive Erweiterung der Auto-DB - 307 zu 829 Eintraege
    - BMW: M2 G87, M2 CS, M3/M5 alle Generationen (E28 bis G90), 4er M440i Varianten, M4 CS/GTS/Cabrio, M635 CSi, 850 CSi, 760i V12, i7 M70, Z8, Z3 M Coupe, 3.0 CSL, 507, 328 Roadster, Alpina B3/B5/B7/XB7/XD3/B8/B12
    - Porsche: alle 911-Generationen (G-Modell, 964, 993, 996, 997, 991.1, 991.2, 992) mit GT3/GT3 RS/Turbo/GT2 RS/Sport Classic/Speedster/R; 924 Carrera GT, 928 GTS, 944 Turbo, 968 CS, 550 Spyder, 356 Speedster, Singer DLS/Reimagined, 911 GT1, 935 (2018)
    - Ferrari: 308/328/348/355/360/430/458/488 Generationen mit Speciale/Pista/Scuderia/16M Varianten, F12 tdf, 599 GTO/SA Aperta, 575M, 550, 456, 612, 250 GT California Spider/Lusso/SWB, Daytona, Dino, Monza SP1/SP2, FXX-K Evo
    - Lamborghini: Gallardo Generationen, Murcielago LP670 SV, Aventador SV/SVJ Roadster, Diablo Varianten, Miura SV, Countach LP400/QV/25th, Espada, Jarama, Islero, Urraco, Silhouette, Jalpa, LM002, Reventon, Veneno, Sesto Elemento, Centenario
    - Mercedes/AMG/Maybach: 300 SL Roadster, Pagode 230/250/280, 600 Pullman, SLS AMG Black Series, SLR Stirling Moss, CLK GTR, 190E Evo I/II, AMG GT R/Black Series, GT 53/63 4-door, C 63 Black, SL 65 Black, G 65, G 6x6, G 4x4 Quadrat
    - Audi: RS2/RS4 Generationen B5-B9, RS5/RS6/RS7 Varianten, R8 V8/V10/GT Generationen, TT RS Iconic, S8, A8 W12, RS6 GT, RS e-tron GT Performance
    - McLaren: F1 GTR/LM, MP4-12C, 540C/570S/600LT/620R/675LT/720S/GT/Solus/Sabre/Elva
    - Aston Martin: DB4/DB5/DB6/DB7/DB9/DB11, V12 Vantage Gen 1/2, Vanquish, One-77, Vulcan, DBR1
    - Hypercars: Spyker, Apollo, Czinger, Saleen, Ascari, Bugatti EB110/T35/T57 Atlantic
    - US Muscle: Shelby Cobra/GT350/GT500, AC Cobra, Plymouth Cuda/Superbird, Pontiac GTO, Buick GNX, DeLorean
    - Japan: Skyline GT-R R32/R33/R34 Varianten, Toyota AE86/Celica GT-Four, NSX-R, RX-7 FD/Spirit R, 787B, Evo VI Tommi M, 22B STI
    - Plus: Volvo 850R/240 Turbo, Saab 99 Turbo, Spyker, Apollo, Czinger, Ascari, TVR, Morgan, Lister, De Tomaso, Iso, Lancia 037/S4
    - 75 Marken total (vorher 50)
    - Brand-Styles fuer 25 neue Marken in brands.js
    - SW bump v27c

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Strategie: cache-first fuer App-Shell + statische Assets, network-first fuer
 // HTML (damit Updates ankommen). PeerJS-CDN wird beim ersten Mal gecached.
 
-const VERSION = 'meins-v4-2026-05-27b';
+const VERSION = 'meins-v4-2026-05-27c';
 const APP_SHELL = [
   './',
   './index.html',
